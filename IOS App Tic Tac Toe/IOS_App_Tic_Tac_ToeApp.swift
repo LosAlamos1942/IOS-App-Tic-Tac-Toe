008ed14d42c15ecb8ea8ed034ece8ba0e85bbbb2
Nerto Kageh <kagehnerto@yahoo.com>
2021-10-28 10:15:09 -0500

    Initial Commit

---
+++ IOS App Tic Tac Toe/IOS_App_Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_App_Tic_Tac_ToeApp.swift
+//  IOS App Tic Tac Toe
+//
+//  Created by Student on 10/28/21.
+//
+
+import SwiftUI
+
+@main
+struct IOS_App_Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
