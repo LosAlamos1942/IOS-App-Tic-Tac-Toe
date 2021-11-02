b1ab53b691c499cb1c2d7f58c16e819e2757cd93
Nerto Kageh <kagehnerto@yahoo.com>
2021-11-02 10:38:37 -0500

    Added Function Xturn

--- IOS App Tic Tac Toe/ContentView.swift
+++ IOS App Tic Tac Toe/ContentView.swift
@@ -8,21 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var moves = Array(repeating: "", count: 9)
+    @State private var xTurn = true
+    
+    
     var body: some View {
         VStack {
             Text("Tic Tac Toe")
                 .padding()
                 .preferredColorScheme(.dark)
             LazyVGrid(columns: Array(repeating: GridItem(.fixed(120), spacing: 15), count: 3), spacing: 15, content: {
-                      Text("X")
-                      Text("X")
-                    Text("X")
-                    Text("X")
-                    Text("X")
-                    Text("X")
-                    Text("X")
-                    Text("X")
-                    Text("X")
+                ForEach(0..<9) { index in
+                    ZStack {
+                    Color.blue
+                        Text(moves[index])
+                            .font(.system(size: 90))
+                            .fontWeight(.heavy)
+                    }
+                    .frame(width: 120, height: 120, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        .cornerRadius(30)
+                    .onTapGesture {
+                        moves[index] = "X"
+                    }
+                }
         
         })
        
